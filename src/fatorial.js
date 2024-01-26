06498fd9d45fb49e016c5b5a661bccec4dbadb5c
d3Yv3r <deyverson@astrotech.solutions>
2024-01-26 08:39:21 -0300

    Criando a função de fatorial

---
+++ src/fatorial.js
@@ -0,0 +1,24 @@
+/*
+  Dentro da pasta src deve existir um arquivo chamado fatorial.js (x)
+  Dentro do arquivo fatorial.js deve existir uma função chamada fatorial (x)
+  A função deve aceitar como argumento um número inteiro positivo (x)
+  A função deve retornar o resultado do cálculo fatorial
+  A mensagem de erro "Função deve receber um argumento do tipo inteiro positivo." deve ser retornada quando
+  A função receber um valor numérico não inteiro
+  A função receber um valor numérico menor que 0
+  A função receber um valor não numérico
+*/
+
+function fatorial(n1) {
+  let resultado = n1;
+
+  if (typeof n1 !== "number" || n1 < 0) {
+    return "Função deve receber um argumento do tipo inteiro positivo.";
+  }
+
+  while (n1 > 1) {
+    n1--;
+    resultado *= n1;
+  }
+  return resultado;
+}
